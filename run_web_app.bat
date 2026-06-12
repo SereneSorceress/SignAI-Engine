6f10d94b4341b8bfc9de4045d603fc3cb75d47f9
Jeevesh-Bisht <jeeveshbisht1@gmail.com>
2026-06-12 11:48:42 +0530

    Update run_web_app.bat

--- run_web_app.bat
+++ run_web_app.bat
@@ -2,6 +2,6 @@
 title SIGN LANGUAGE ENGINE - WEB DASHBOARD
 cd /d D:\SIGN-LANGUAGE-PROJECT
 call venv\Scripts\activate
-echo 🔥 Launching Web UI Dashboard...
+echo Launching Web UI Dashboard...
 streamlit run app_web.py
 pause
